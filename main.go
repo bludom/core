8d9b9b6abdfc31683cdc07720dd2b0ce13f2f4bc
Volker Schmitz <v.schmitz@tarent.de>
2018-09-04 20:43:56 +0200

    add persistence

--- main.go
+++ main.go
@@ -1,28 +1,64 @@
 package main
 
 import (
+	"strconv"
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
+type Temperature struct {
+	ID int
+	Temperature float32
+}
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/temperature", TemperaturePutHandler).Methods("PUT", "POST")
+	r.HandleFunc("/temperature/{id}", TemperatureGetHandler).Methods("GET")
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		panic(err)
 	}
 }
 
-// HomeHandler ...
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	body := []byte{}
+// TemperaturePutHandler ...
+func TemperaturePutHandler(w http.ResponseWriter, r *http.Request) {
+	body := make([]byte, r.ContentLength)
+
+	var temp Temperature
 
 	w.WriteHeader(http.StatusOK)
 	r.Body.Read(body)
 
-	fmt.Println(body)
+	json.Unmarshal(body, &temp)
+
+	if err := save(temp); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println(string(body))
+}
+
+// TemperatureGetHandler ...
+func TemperatureGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	temperature, err := get(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(temperature))
+
 }
 
---
+++ persistence.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	bolt "go.etcd.io/bbolt"
+)
+
+func save(temp Temperature) error {
+
+	db, err := bolt.Open("temperature.db", 0600, nil)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	if err := db.Update(func(tx *bolt.Tx) error{
+		tempBucket, err := tx.CreateBucketIfNotExists([]byte("temperature"))
+		if err != nil {
+			return err
+		}
+
+		return tempBucket.Put([]byte(fmt.Sprintf("%d", temp.ID)), []byte(fmt.Sprintf("%f", temp.Temperature)))
+
+	}); err != nil {
+		panic(err)
+	}
+	return nil
+}
+
+func get(id int) ( result []byte, err error ) {
+
+	db, err := bolt.Open("temperature.db", 0600, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	defer db.Close()
+	if err := db.View(func(tx *bolt.Tx) error{
+		result = tx.Bucket([]byte("temperature")).Get([]byte(fmt.Sprintf("%d", id)))
+		return nil
+	}); err != nil {
+		panic(err)
+	}
+	return result, nil
+}
